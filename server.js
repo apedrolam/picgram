1462cc427a219c8e263f6b0ab9dd2c764b0ea70f
Albert Pedrola <abert.pedrola@netcentric.biz>
2017-07-05 16:37:45 +0200

    Add pug and require CSS from server.js

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express();
 
+app.set('view engine', 'pug');
+
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.render('index')
 })
 
 app.listen(3000, function(err) {
